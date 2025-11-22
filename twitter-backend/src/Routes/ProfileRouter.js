cf5df9457c25bf80f08b963ca55a643efeb8c5c0
Akash Das <akashakki1100@gmail.com>
2025-11-22 19:10:55 +0530

    edit profile api

--- twitter-backend/src/Routes/ProfileRouter.js
+++ twitter-backend/src/Routes/ProfileRouter.js
@@ -1,13 +1,17 @@
 const express = require("express")
 const { isLoggedIn } = require("../Middleware/isLoggedIn")
+const { User } = require("../Models/User")
 const router = express.Router()
 
 
 router.patch("/profile/edit", isLoggedIn, async (req, res) => {
     try {
-        res.json({msg: "done"})
+        const userId = req.user._id
+        const {firstName, lastName, bio, profilePicture} = req.body
+        const updateProfile = await User.findByIdAndUpdate(userId, {firstName, lastName, bio, profilePicture}, {new: true}).select("firstName lastName bio profilePicture dateOfBirth username post followers following")
+        res.status(200).json({msg: "done", data: updateProfile})
     } catch (error) {
-        
+        res.status(400).json({error: error.message})
     }
 })
 
